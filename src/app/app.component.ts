7aca563915fc0aaf5dd997a6e75fb0db82498216
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-07 17:30:33 +0100

    repeat component usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {UserComponent} from './user/user.component';
+import {DUMMY_USERS} from './data/dummy-users-data';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
 }
