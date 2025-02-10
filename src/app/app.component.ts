7daf824048cb85caff9d8eb58d33aa4b9b112e5f
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-10 17:07:55 +0100

    using User model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import {User} from './model/AppGenericModels';
 })
 export class AppComponent {
   users: User[] = DUMMY_USERS;
-  selectedUserId: string = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find(user => user.id === this.selectedUserId);
