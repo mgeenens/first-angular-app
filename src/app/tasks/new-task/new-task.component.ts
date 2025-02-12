5e42f0c632d3edf6599abedeb4bff201bd38eb9f
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-12 17:20:54 +0100

    new task form ngModel

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,18 +1,29 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
   @Input({required: true}) userId!: string;
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+
   }
 
 }
