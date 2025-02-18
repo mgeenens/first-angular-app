2365651f5c1d19eea2e1cfe73f1a2c9a2ad57242
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-18 17:33:42 +0100

    reset project to practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,8 @@
-import {Component} from '@angular/core';
-import {DUMMY_USERS} from './data/dummy-users-data';
-import {User} from './model/app.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  users: User[] = DUMMY_USERS;
-  selectedUserId?: string;
-
-  get selectedUser() {
-    return this.users.find(user => user.id === this.selectedUserId);
-  }
-
-  onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
-    this.selectedUserId = id;
-  }
-
-}
+export class AppComponent {}
