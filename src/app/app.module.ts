ef86f860c6fe4ae1d42a4d76b1cdc9660e18dfb6
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-19 15:27:27 +0100

    adding header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {BrowserModule} from '@angular/platform-browser';
+import {HeaderComponent} from './header/header.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule
   ],
-  exports: [AppComponent]
+  exports: [
+    AppComponent,
+    HeaderComponent
+  ]
 })
 export class AppModule {
 }
