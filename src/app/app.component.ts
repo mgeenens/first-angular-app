cabda85d1f353711c6609447b940aa406913cff1
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-20 16:35:16 +0100

    refacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from './header/header.component';
-import {ServerStatusComponent} from './server-status/server-status.component';
-import {TrafficComponent} from './traffic/traffic.component';
-import {SupportTicketComponent} from './support-ticket/support-ticket.component';
+import {ServerStatusComponent} from './dashboard/server-status/server-status.component';
+import {TrafficComponent} from './dashboard/traffic/traffic.component';
+import {SupportTicketComponent} from './dashboard/support-ticket/support-ticket.component';
 
 @Component({
   selector: 'app-root',
