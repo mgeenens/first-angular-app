5324ab1614b64ff59d0e2edc7b8437fbe03062a5
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-20 15:14:53 +0100

    spliting components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from './header/header.component';
+import {ServerStatusComponent} from './server-status/server-status.component';
+import {TrafficComponent} from './traffic/traffic.component';
+import {SupportTicketComponent} from './support-ticket/support-ticket.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    SupportTicketComponent
+  ]
 })
 export class AppComponent {
   dummyTrafficData = [
---
+++ src/app/support-ticket/support-ticket.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-support-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-ticket.component.html',
+  styleUrl: './support-ticket.component.css'
+})
+export class SupportTicketComponent {
+}
