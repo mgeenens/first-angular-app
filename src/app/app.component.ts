5e49ba56de1c7cad3fb995f47daa106f0507d1a5
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-20 15:01:00 +0100

    starting new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,41 @@
 import { Component } from '@angular/core';
-import {InvestmentData, InvestmentResult} from './model/app.model';
-import {InvestmentResultService} from './service/investment-result.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-
-  resultsData?: InvestmentResult[];
-
-  constructor(private readonly investmentResultService: InvestmentResultService) {
-  }
-
-  onCalculate(data: InvestmentData) {
-    this.resultsData = this.investmentResultService.calculateInvestmentResults(data);
-  }
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
 }
