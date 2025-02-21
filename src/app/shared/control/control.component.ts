929725156a6d1e94a0ea11392fed69b461166a54
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-02-21 16:05:09 +0100

    accessing dom

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, HostBinding, inject, input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,20 @@ import {Component, input, ViewEncapsulation} from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+
+  // @HostBinding('class') className = 'control';
+
   label = input<string>();
+  private readonly el = inject(ElementRef);
+
+  onClick() {
+    console.log("Clicked !");
+    console.log(this.el);
+  }
 
 }
