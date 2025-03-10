a7227145d9fbb2a55244fd471bde3c2a1008901c
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-03-10 17:17:20 +0100

    section6 | template variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     clearTimeout(this.interval);
-
   }
 
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ButtonComponent} from '../../../shared/button/button.component';
 import {ControlComponent} from '../../../shared/control/control.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   imports: [
     ButtonComponent,
-    ControlComponent
+    ControlComponent,
+    FormsModule
   ],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
@@ -15,5 +17,10 @@ import {ControlComponent} from '../../../shared/control/control.component';
 export class NewTicketComponent {
   titleLabel: string = 'Title';
   textareaLabel: string = 'Request';
+
+  onSubmit(title: string, text: string) {
+    console.log("ENTERED TITLE :" + title);
+    console.log("ENTERED TEXT :" + text);
+  }
 
 }
