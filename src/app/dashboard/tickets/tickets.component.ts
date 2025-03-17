4eec98903371f7130c036fd64368d90c715cc9ed
Maxime GEENENS <maxime.geenens@conserto.pro>
2025-03-17 16:03:18 +0100

    dynamic ticket styling

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {NewTicketComponent} from './new-ticket/new-ticket.component';
 import {Ticket} from '../../model/ticket.model';
+import {TicketComponent} from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
   imports: [
-    NewTicketComponent
+    NewTicketComponent,
+    TicketComponent
   ],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
@@ -24,5 +26,14 @@ export class TicketsComponent {
     }
 
     this.tickets.push(ticket);
+  }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return {...ticket, status: 'closed'}
+      }
+      return ticket;
+    });
   }
 }
